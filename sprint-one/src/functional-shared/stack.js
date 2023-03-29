daf3c949d9aa1c5551cfcde4653ae9215f0eb7b4
Milad Moulayi <m.moulayi123@gmail.com>
2023-03-29 15:31:12 -0700

    Cleaned up functional/stack.js and functional-shared/stack.js

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,12 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var storage = {};
-  var idx = 0;
 
   var someInstance = {
-    storage: storage,
-    idx: idx
+    storage: {},
+    idx: 0
   };
 
   _.extend(someInstance, stackMethods);
@@ -18,7 +16,6 @@ var stackMethods = {};
 
 stackMethods.push = function(value) {
   this.storage[this.idx] = value;
-  // this.someInstance[this.idx] = value;
   this.idx++;
 }
 
@@ -26,7 +23,6 @@ stackMethods.pop = function() {
   this.idx--;
   var last = this.storage[this.idx];
   delete this.storage[this.idx];
-  // delete this.someInstance[this.idx];
   return last;
 }
 
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,21 +5,16 @@ var Stack = function() {
   var storage = {};
   var idx = 0;
 
-
   // Implement the methods below
   someInstance.push = function(value) {
-    if (storage[idx] === undefined) {
-      storage[idx] = value;
-      someInstance[idx] = value;
-      idx++;
-    }
+    storage[idx] = value;
+    idx++;
   };
 
   someInstance.pop = function() {
     idx--;
     var last = storage[idx];
     delete storage[idx];
-    delete someInstance[idx];
     return last;
   };
 
