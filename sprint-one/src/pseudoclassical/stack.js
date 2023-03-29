2a8e2b77c699253e643917ef2bc349332637ed4f
Milad Moulayi <m.moulayi123@gmail.com>
2023-03-29 15:24:04 -0700

    Completed pseudoclassical/stack.js

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.storage = {};
+  this.idx = 0;
+
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.idx] = value;
+  this.idx++;
+}
 
+Stack.prototype.pop = function() {
+  this.idx--;
+  var last = this.storage[this.idx];
+  delete this.storage[this.idx];
+  return last;
+}
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+}
